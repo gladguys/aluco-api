02f9d892c9c144de9bd3c8b729274441030d3084
denis <dineresc@gmail.com>
2019-10-18 02:15:30 -0300

    up

---
+++ src/main/java/com/gladguys/alucoapi/AlucoApiApplication.java
@@ -0,0 +1,13 @@
+package com.gladguys.alucoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlucoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlucoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gladguys/alucoapi/AlucoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gladguys.alucoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlucoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
