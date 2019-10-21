96a30c208d2f1decb8841988ff34165e645aed17
Lailson Bento <lailson.contatos@gmail.com>
2019-10-21 00:33:42 -0300

    add Grade Repository and Service

---
+++ src/main/java/com/gladguys/alucoapi/services/impl/GradeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.gladguys.alucoapi.services.impl;
+
+import com.gladguys.alucoapi.entities.Grade;
+import com.gladguys.alucoapi.services.GradeService;
+
+public class GradeServiceImpl implements GradeService {
+
+	@Override
+	public Grade getById(Long id) {
+		return null;
+	}
+
+	@Override
+	public Grade update(Grade grade) {
+		return null;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+
+	}
+
+}
