1f6d7d6868017e8ddcd62c094bbd3762c84e49b9
Lailson Bento <lailson.contatos@gmail.com>
2019-10-22 00:14:32 -0300

    add exceptions

--- src/main/java/com/gladguys/alucoapi/exception/ResponseException.java
+++ src/main/java/com/gladguys/alucoapi/exception/ResponseException.java
@@ -1,4 +1,26 @@
 package com.gladguys.alucoapi.exception;
 
-public class ResponseException {
+import org.springframework.http.HttpStatus;
+
+public class ResponseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String message;
+	private final HttpStatus httpStatus;
+
+	public ResponseException(String message, HttpStatus httpStatus) {
+		this.message = message;
+		this.httpStatus = httpStatus;
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	public HttpStatus getHttpStatus() {
+		return httpStatus;
+	}
+
 }
