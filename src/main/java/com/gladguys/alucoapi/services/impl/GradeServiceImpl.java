55471504dd05ca3fc05c4399f9fc4774411bd4fb
denis <dineresc@gmail.com>
2019-10-22 22:03:24 -0300

    creating layers service and repository

--- src/main/java/com/gladguys/alucoapi/services/impl/GradeServiceImpl.java
+++ src/main/java/com/gladguys/alucoapi/services/impl/GradeServiceImpl.java
@@ -1,25 +1,52 @@
 package com.gladguys.alucoapi.services.impl;
 
 import com.gladguys.alucoapi.entities.Grade;
+import com.gladguys.alucoapi.entities.dto.GradeDTO;
+import com.gladguys.alucoapi.repositories.GradeRepository;
 import com.gladguys.alucoapi.services.GradeService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class GradeServiceImpl implements GradeService {
 
-	@Override
-	public Grade getById(Long id) {
-		return null;
+	private GradeRepository gradeRepository;
+
+	public GradeServiceImpl(GradeRepository gradeRepository) {
+		this.gradeRepository = gradeRepository;
 	}
 
 	@Override
-	public Grade update(Grade grade) {
-		return null;
+	public Grade getById(Long id) throws Exception {
+
+		if (id == null) throw new Exception("id null");
+
+		return this.gradeRepository.findById(id).orElse(null);
+
 	}
 
 	@Override
-	public void deleteById(Long id) {
+	public Set<Grade> getAllGradesByExam(Long examId) throws Exception {
 
+		if(examId == null) throw new Exception("id da turma não informado");
+
+		return this.gradeRepository.getAllByExamIdOrderByStudentId(examId);
+
+	}
+
+	@Override
+	public Grade saveOrUpdate(GradeDTO gradeDTO) {
+		return this.gradeRepository.save(gradeDTO.toEntity());
+	}
+
+	@Override
+	public void deleteById(Long id) throws Exception {
+
+		if(id == null) throw new Exception("id null");
+
+		this.gradeRepository.deleteById(id);
 	}
 
 }
