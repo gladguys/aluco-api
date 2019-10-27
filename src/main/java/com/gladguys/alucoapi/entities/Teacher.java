42df5623a9c34a74b13599166536bf32cc661597
Lailson Bento <lailson.contatos@gmail.com>
2019-10-27 00:35:01 -0300

    refatoring

--- src/main/java/com/gladguys/alucoapi/entities/Teacher.java
+++ src/main/java/com/gladguys/alucoapi/entities/Teacher.java
@@ -21,7 +21,6 @@ public class Teacher {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotBlank(message = "Nome deve ser informado.")
 	private String name;
 
 	@Column(name = "photo_url")
@@ -35,7 +34,4 @@ public class Teacher {
 	@JoinColumn(name = "user_aluco_id")
 	private User user;
 
-	public void hideUserPassword() {
-		this.user.setPassword(null);
-	}
 }
