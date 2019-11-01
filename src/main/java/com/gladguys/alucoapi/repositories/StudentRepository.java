3a2ac07636bf41d4e0206f98f548f38d3b1c4526
Lailson Bento <lailson.contatos@gmail.com>
2019-11-01 00:41:42 -0300

    update to jdbc

--- src/main/java/com/gladguys/alucoapi/repositories/StudentRepository.java
+++ src/main/java/com/gladguys/alucoapi/repositories/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.gladguys.alucoapi.repositories;
 
 import com.gladguys.alucoapi.entities.Student;
+import com.gladguys.alucoapi.repositories.customs.CustomStudentRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,13 +10,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Set;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long>, CustomStudentRepository {
 
     //@Query("select s from Student s where s.teacher.id = ?1 order by s.name")
     Set<Student> findAllByTeacherIdOrderByName(Long teacherId);
 
-    @Query(value = "SELECT s.* FROM student s " +
-           "INNER JOIN student_class sc ON sc.student_id = s.id " +
-           "WHERE sc.class_id = :id", nativeQuery = true)
-    Set<Student> getAllByClassId(@Param("id") Long classId);
 }
