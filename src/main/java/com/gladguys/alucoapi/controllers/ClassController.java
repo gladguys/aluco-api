7ef8ea05f5c522808654f0d494caee34c60d94c0
Lailson Bento <lailson.contatos@gmail.com>
2019-11-09 14:45:57 -0300

    remove try-catch and throw response exception - class controller

--- src/main/java/com/gladguys/alucoapi/controllers/ClassController.java
+++ src/main/java/com/gladguys/alucoapi/controllers/ClassController.java
@@ -3,6 +3,7 @@ package com.gladguys.alucoapi.controllers;
 import com.gladguys.alucoapi.entities.StudentWrapper;
 import com.gladguys.alucoapi.entities.dto.ClassDTO;
 import com.gladguys.alucoapi.entities.dto.StudentDTO;
+import com.gladguys.alucoapi.exception.ApiResponseException;
 import com.gladguys.alucoapi.security.jwt.JwtTokenUtil;
 import com.gladguys.alucoapi.services.ClassService;
 import com.gladguys.alucoapi.services.StudentService;
@@ -39,138 +40,94 @@ public class ClassController {
 	@ApiOperation(value = "Retorna as turmas de um professor específico")
 	@GetMapping("/teacher/{teacherId}")
 	public ResponseEntity<List<ClassDTO>> getAllByTeacher(@PathVariable("teacherId") Long teacherId) {
-		try {
-			if(teacherId == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		if (teacherId == null) throw new ApiResponseException("Professor é obrigatório");
 
-			List<ClassDTO> classes = this.classService.getAllByTeacher(teacherId);
-			return ResponseEntity.ok(classes);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		List<ClassDTO> classes = this.classService.getAllByTeacher(teacherId);
+		return ResponseEntity.ok(classes);
 	}
 
 	@ApiOperation(value = "Retorna as turmas do professor logado")
 	@GetMapping
 	public ResponseEntity<List<ClassDTO>> getAll(HttpServletRequest request) {
-		try {
-			Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
-			List<ClassDTO> classes = this.classService.getAllByTeacher(teacherId);
-			return ResponseEntity.ok(classes);
+		Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
+		List<ClassDTO> classes = this.classService.getAllByTeacher(teacherId);
 
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		return ResponseEntity.ok(classes);
 	}
 
 	@ApiOperation(value = "Retorna uma turma pelo código")
 	@GetMapping("/{id}")
 	public ResponseEntity<ClassDTO> getById(@PathVariable("id") Long id) {
-		try {
-			if(id == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		if (id == null) throw new ApiResponseException("Turma é obrigatória");
 
-			ClassDTO classFound = this.classService.getById(id);
-			return ResponseEntity.ok(classFound);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		ClassDTO classFound = this.classService.getById(id);
+		return ResponseEntity.ok(classFound);
 	}
 
 	@ApiOperation(value = "Retorna os estudantes de uma turma específica")
 	@GetMapping("/{id}/students")
 	public ResponseEntity<List<StudentDTO>> getStudentsByClassId(@PathVariable("id") Long id) {
-		try{
-			if(id == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		if (id == null) throw new ApiResponseException("Turma é obrigatória");
 
-			List<StudentDTO> students = this.studentService.getAllByClassId(id);
-			return ResponseEntity.ok(students);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		List<StudentDTO> students = this.studentService.getAllByClassId(id);
+		return ResponseEntity.ok(students);
 	}
 
 	@ApiOperation(value = "Cadastra uma turma")
 	@PostMapping
 	public ResponseEntity<ClassDTO> save(@RequestBody ClassDTO dto, HttpServletRequest request) {
-		try {
-			if(dto == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		if (dto == null) throw new ApiResponseException("Turma é obrigatória");
 
-			Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
-			dto.setTeacherId(teacherId);
+		Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
+		dto.setTeacherId(teacherId);
 
-			ClassDTO classDTO = this.classService.saveOrUpdate(dto);
-
-			return ResponseEntity.status(HttpStatus.CREATED).body(classDTO);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		ClassDTO classDTO = this.classService.saveOrUpdate(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(classDTO);
 	}
 
 	@ApiOperation(value = "Atualiza uma turma")
 	@PutMapping
 	public ResponseEntity<ClassDTO> update(@RequestBody ClassDTO dto, HttpServletRequest request) {
+		if (dto == null || dto.getId() == null) throw new ApiResponseException("Turma é obrigatória");
 
-		try {
-			if(dto == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-			else if (dto.getId() == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		boolean exists = this.classService.exists(dto.getId());
+		if (!exists) throw new ApiResponseException("Turma não encontrada");
 
-			boolean exists = this.classService.exists(dto.getId());
-			if (!exists) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
+		dto.setTeacherId(teacherId);
 
-			Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
-			dto.setTeacherId(teacherId);
-
-			ClassDTO classSaved = this.classService.saveOrUpdate(dto);
-			return ResponseEntity.ok(classSaved);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		ClassDTO classSaved = this.classService.saveOrUpdate(dto);
+		return ResponseEntity.ok(classSaved);
 	}
 
 	@ApiOperation(value = "Deleta uma turma")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> delete(@PathVariable("id") Long id) {
-		try {
-			boolean exists = this.classService.exists(id);
-			if (!exists) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		boolean exists = this.classService.exists(id);
+		if (!exists) throw new ApiResponseException("Turma não encontrada");
 
-			this.classService.deleteById(id);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		this.classService.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 
 	@ApiOperation(value = "Cadastra uma lista de estudantes em uma turma")
 	@PostMapping("/{id}/students")
 	public ResponseEntity saveStudentsForClass(@PathVariable("id") Long id, @RequestBody StudentWrapper wrapper) {
-		try {
-			if(id == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-			if(wrapper == null) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		if (id == null) throw new ApiResponseException("Turma é obrigatória");
+		if (wrapper == null) throw new ApiResponseException("Aluno(s) obrigatório(s)");
 
-			this.classService.addStudentsIntoClass(wrapper.getStudentDTOS(), id);
-			return ResponseEntity.status(HttpStatus.CREATED).body(null);
-
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		this.classService.addStudentsIntoClass(wrapper.getStudentDTOS(), id);
+		return ResponseEntity.status(HttpStatus.CREATED).body(null);
 	}
 
 	@ApiOperation("Desvincula aluno de uma turma")
 	@DeleteMapping("{classId}/students/{studentId}")
 	public ResponseEntity deleteStudentFromClass(@PathVariable("classId") Long classId, @PathVariable("studentId") Long studentId) {
-		try {
-			if (studentId == null && classId == null) return ResponseEntity.badRequest().body(null);
+		if (studentId == null) throw new ApiResponseException("Aluno é obrigatório");
+		if (classId == null) throw new ApiResponseException("Turma é obrigatória");
 
-			this.classService.deleteStudentFromClass(studentId, classId);
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-		}
+		this.classService.deleteStudentFromClass(studentId, classId);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
 	}
 
 }
