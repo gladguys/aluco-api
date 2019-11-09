74815dea578c36b93e7f379af1bc2e656fe2e056
Lailson Bento <lailson.contatos@gmail.com>
2019-11-09 14:52:39 -0300

    removing imports, blank lines and objects useless

--- src/main/java/com/gladguys/alucoapi/services/impl/ClassServiceImpl.java
+++ src/main/java/com/gladguys/alucoapi/services/impl/ClassServiceImpl.java
@@ -1,15 +1,12 @@
 package com.gladguys.alucoapi.services.impl;
 
 import com.gladguys.alucoapi.entities.Class;
-import com.gladguys.alucoapi.entities.Exam;
 import com.gladguys.alucoapi.entities.Student;
 import com.gladguys.alucoapi.entities.dto.ClassDTO;
-import com.gladguys.alucoapi.entities.dto.ExamDTO;
 import com.gladguys.alucoapi.entities.dto.StudentDTO;
 import com.gladguys.alucoapi.repositories.ClassRepository;
 import com.gladguys.alucoapi.services.ClassService;
 import com.gladguys.alucoapi.services.ExamService;
-import com.gladguys.alucoapi.services.StudentService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -17,18 +14,15 @@ import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ClassServiceImpl implements ClassService {
 
 	private ClassRepository classRepository;
-	private StudentService studentService;
 	private ExamService examService;
 
-	public ClassServiceImpl(ClassRepository classRepository, StudentService studentService, ExamService examService) {
+	public ClassServiceImpl(ClassRepository classRepository, ExamService examService) {
 		this.classRepository = classRepository;
-		this.studentService = studentService;
 		this.examService = examService;
 	}
 
--- src/main/java/com/gladguys/alucoapi/services/impl/ExamServiceImpl.java
+++ src/main/java/com/gladguys/alucoapi/services/impl/ExamServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ExamServiceImpl implements ExamService {
