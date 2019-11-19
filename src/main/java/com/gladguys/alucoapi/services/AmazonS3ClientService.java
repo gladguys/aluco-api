a44d0746db0b48f98a986a2d7873ca1d26a6c4b8
denis <dineresc@gmail.com>
2019-11-19 00:14:55 -0300

    creating service for loading file

--- src/main/java/com/gladguys/alucoapi/services/AmazonS3ClientService.java
+++ src/main/java/com/gladguys/alucoapi/services/AmazonS3ClientService.java
@@ -1,4 +1,10 @@
 package com.gladguys.alucoapi.services;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface AmazonS3ClientService {
+	void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess) throws IOException;
+	void deleteFileFromS3Bucket(String fileName);
 }
--- src/main/java/com/gladguys/alucoapi/services/impl/AmazonS3ClientServiceImpl.java
+++ src/main/java/com/gladguys/alucoapi/services/impl/AmazonS3ClientServiceImpl.java
@@ -1,4 +1,68 @@
 package com.gladguys.alucoapi.services.impl;
 
-public class AmazonS3ClientServiceImpl {
-}
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.gladguys.alucoapi.services.AmazonS3ClientService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Component
+public class AmazonS3ClientServiceImpl implements AmazonS3ClientService {
+	private String awsS3AudioBucket;
+	private AmazonS3 amazonS3;
+
+	@Autowired
+	public AmazonS3ClientServiceImpl(Region awsRegion, AWSCredentialsProvider awsCredentialsProvider, String awsS3AudioBucket)
+	{
+		this.amazonS3 = AmazonS3ClientBuilder.standard()
+				.withCredentials(awsCredentialsProvider)
+				.withRegion(awsRegion.getName()).build();
+		this.awsS3AudioBucket = awsS3AudioBucket;
+	}
+
+	@Async
+	public void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess) throws IOException {
+		String fileName = multipartFile.getOriginalFilename();
+
+		try {
+			//creating the file in the server (temporarily)
+			File file = new File(fileName);
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(multipartFile.getBytes());
+			fos.close();
+
+			PutObjectRequest putObjectRequest = new PutObjectRequest(this.awsS3AudioBucket, fileName, file);
+
+			if (enablePublicReadAccess) {
+				putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead);
+			}
+			this.amazonS3.putObject(putObjectRequest);
+			//removing the file created in the server
+			file.delete();
+		} catch (IOException | AmazonServiceException ex) {
+		}
+	}
+
+	@Async
+	public void deleteFileFromS3Bucket(String fileName)
+	{
+		try {
+			amazonS3.deleteObject(new DeleteObjectRequest(awsS3AudioBucket, fileName));
+		} catch (AmazonServiceException ex) {
+		}
+	}}
