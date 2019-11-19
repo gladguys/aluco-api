e3651dcd370c7e289f092fe4ac08f955e6f2a7dd
denis <dineresc@gmail.com>
2019-11-19 00:26:11 -0300

    add variables

--- src/main/java/com/gladguys/alucoapi/controllers/FileHandlerController.java
+++ src/main/java/com/gladguys/alucoapi/controllers/FileHandlerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/profile")
+@RequestMapping("/api/profiless")
 public class FileHandlerController {
 
 	@Autowired
