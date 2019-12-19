09cba773a81ef3eb7451ea5c6163a47660e4b023
Luan Lima <luanlima.olliveir@gmail.com>
2019-12-19 17:06:41 -0300

    Criado API StudentClassObservationController

---
+++ src/main/java/com/gladguys/alucoapi/controllers/StudentClassObservationController.java
@@ -0,0 +1,46 @@
+package com.gladguys.alucoapi.controllers;
+
+import java.util.List;
+
+import com.gladguys.alucoapi.entities.StudentClassObservation;
+import com.gladguys.alucoapi.entities.dto.StudentClassObservationDTO;
+import com.gladguys.alucoapi.security.jwt.JwtTokenUtil;
+import com.gladguys.alucoapi.services.StudentClassObservationService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/api/observations/student")
+public class StudentClassObservationController {
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    private StudentClassObservationService studentClassObservationService;
+
+    public StudentClassObservationController(StudentClassObservationService studentClassObservationService, JwtTokenUtil jwtTokenUtil) {
+        this.studentClassObservationService = studentClassObservationService;
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @ApiOperation(value = "Salva as observações realizadas pelo professor em sala de aula de um determinado estudante")
+    @PostMapping
+    public ResponseEntity<StudentClassObservation> save(HttpServletRequest request, StudentClassObservationDTO studentClassObservationDTO) {
+        Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
+        StudentClassObservation studentClassObservation = this.studentClassObservationService.save(studentClassObservationDTO.toEntity());
+
+        return ResponseEntity.ok(studentClassObservation);
+    }
+
+    @ApiOperation(value = "Retorna as observações realizadas pelo professor em sala de aula de um estudante especifico a partir do seu id")
+    @GetMapping("{studentId}")
+    public ResponseEntity<List<StudentClassObservation>> get(HttpServletRequest request, @PathVariable("studentId") Long studentId) {
+        Long teacherId = jwtTokenUtil.getTeacherIdFromToken(request).longValue();
+        List<StudentClassObservation> studentClassObservationList = this.studentClassObservationService.getStudentObservation(studentId);
+
+        return ResponseEntity.ok(studentClassObservationList);
+    }
+}
