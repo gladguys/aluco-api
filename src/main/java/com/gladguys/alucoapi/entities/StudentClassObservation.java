cf8d2f2d6e4603535875bcd7dcc472d38d48d4fa
Luan Lima <luanlima.olliveir@gmail.com>
2019-12-19 16:55:33 -0300

    Criada entidade StudentClassObservation

---
+++ src/main/java/com/gladguys/alucoapi/entities/StudentClassObservation.java
@@ -0,0 +1,27 @@
+package com.gladguys.alucoapi.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity(name = "student_class_observation")
+public class StudentClassObservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private Class classStudent;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate date;
+    private String observation;
+}
