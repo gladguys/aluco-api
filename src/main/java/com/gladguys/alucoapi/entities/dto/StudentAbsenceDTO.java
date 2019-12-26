92b7b974858ef7059ce5c4e2c934bbcd4f8e4a51
Denis Caminha <dineresc@gmail.com>
2019-12-26 16:35:36 -0300

    Update StudentAbsenceDTO.java

--- src/main/java/com/gladguys/alucoapi/entities/dto/StudentAbsenceDTO.java
+++ src/main/java/com/gladguys/alucoapi/entities/dto/StudentAbsenceDTO.java
@@ -3,6 +3,7 @@ package com.gladguys.alucoapi.entities.dto;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
