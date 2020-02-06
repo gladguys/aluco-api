42ffc4fac68b4e7cda02f8efdc35bf3fb12d20a5
denis <dineresc@gmail.com>
2020-02-06 01:30:02 -0300

    creating classtatus enum

--- src/main/java/com/gladguys/alucoapi/entities/enums/ClassStatus.java
+++ src/main/java/com/gladguys/alucoapi/entities/enums/ClassStatus.java
@@ -1,4 +1,22 @@
 package com.gladguys.alucoapi.entities.enums;
 
 public enum ClassStatus {
+	CREATED(0),
+	STARTED(1),
+	CLOSED(2);
+
+	Integer id;
+
+	ClassStatus(Integer id) {
+		this.id = id;
+	}
+
+	public static ClassStatus getStatus(Integer id) {
+		switch (id) {
+			case 0: return CREATED;
+			case 1: return STARTED;
+			case 2: return CLOSED;
+			default: return null;
+		}
+	}
 }
