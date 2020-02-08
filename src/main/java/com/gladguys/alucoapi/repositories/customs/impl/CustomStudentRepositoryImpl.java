1f07b46ce446275ad4496a6295efa43805d78fb6
denis <dineresc@gmail.com>
2020-02-08 10:41:45 -0300

    add left join

--- src/main/java/com/gladguys/alucoapi/repositories/customs/impl/CustomStudentRepositoryImpl.java
+++ src/main/java/com/gladguys/alucoapi/repositories/customs/impl/CustomStudentRepositoryImpl.java
@@ -23,7 +23,7 @@ public class CustomStudentRepositoryImpl implements CustomStudentRepository {
 		sql.append("SELECT s.id, s.gender, s.name, s.aee, s.registration_number as registrationNumber, ");
 		sql.append(" nc.number as numberCall FROM student s ");
 		sql.append(" INNER JOIN student_class sc ON sc.student_id = s.id ");
-		sql.append(" INNER JOIN number_call nc ON nc.student_id = s.id and nc.class_id = ? ");
+		sql.append(" LEFT JOIN number_call nc ON nc.student_id = s.id and nc.class_id = ? ");
 		sql.append(" WHERE sc.class_id = ? ");
 		sql.append(" ORDER BY (s.name) ");
 
